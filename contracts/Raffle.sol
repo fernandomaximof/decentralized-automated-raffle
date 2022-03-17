9e176135490e4c9041965a163e71a4006197b04f
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-03-17 22:15:14 +1300

    added tests

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,26 +9,25 @@ contract Raffle {
 
     function enterRaffle() public payable {
         require(msg.value == s_entranceFee, "PLEASE SEND 10 GWEI");
-        
-        s_players.push(payable(msg.sender));
 
+        s_players.push(payable(msg.sender));
         //limit number of players?
     }
 
     function chooseWinner() public {
         uint256 randomWinner = 0;
         address payable winner = s_players[randomWinner];
+        
         s_recentWinner = winner;
+        
         (bool success,) = winner.call{value: address(this).balance}("");
+        
         require(success, "TRANSFER TO WINNER FAILED");
         delete s_players;
     }
 
     function contractStatus() public view returns(uint256, uint256) {
-        return (
-            address(this).balance,
-            s_players.length
-        );
+        return (address(this).balance, s_players.length);
     }
 
 }
---
+++ test/raffle-test.js
@@ -0,0 +1,43 @@
+const { expect } = require("chai");
+var web3 = require('web3');
+
+describe("RAFFLE CONTRACT", function() {
+    var raffle;
+    var owner
+    var addr1;
+    var addr2;
+    const raffleFee = web3.utils.toWei("10", "Gwei");
+  
+    before(async function() {
+        [owner, addr1, addr2] = await ethers.getSigners();
+
+        const Raffle = await ethers.getContractFactory("Raffle");
+        raffle = await Raffle.deploy();
+        await raffle.deployed();
+    
+    });
+
+    
+    it("ADDRESS 1 ENTER RAFFLE", async function() {
+        await raffle.connect(addr1).enterRaffle({value: raffleFee});
+    });
+
+    it("RETURN CONTRACT STATUS", async function() {
+        status = await raffle.contractStatus()
+        console.log('BALANCE:', BigInt(status[0]), 'NUMBER OF PLAYERS:', BigInt(status[1]))
+    });
+    
+    it("CHOOSE WINNER", async function() {
+        await raffle.chooseWinner();
+    });
+
+    it("RETURN CONTRACT STATUS", async function() {
+        status = await raffle.contractStatus()
+        console.log('BALANCE:', BigInt(status[0]), 'NUMBER OF PLAYERS:', BigInt(status[1]))
+    });
+
+    // it('SHOULD RETURN CORRECT OWNER BALANCE', async function () { 
+    //     expect(BigInt(await bznft.balanceOf(accounts[0])).toString()).to.equal("0");    
+    // });
+
+});
